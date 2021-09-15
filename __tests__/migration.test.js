ef0136023ba57871e7b0de3ba83d41bb84c8a98d
whoastonic <kingstonicthe1@gmail.com>
2021-09-15 05:08:38 -0400

    forget to add a couple

---
+++ __tests__/migration.test.js
@@ -0,0 +1,18 @@
+const test = require('ava')
+const {
+  runMigration,
+  runMigrations,
+  generateMigration,
+} = require('../lib/managers/migration')
+
+test('generate migration', async (t) => {
+  t.pass('unimplemented!')
+})
+
+test('run migrations', async (t) => {
+  t.pass('unimplemented!')
+})
+
+test('rollback migrations', async (t) => {
+  t.pass('unimplemented!')
+})
